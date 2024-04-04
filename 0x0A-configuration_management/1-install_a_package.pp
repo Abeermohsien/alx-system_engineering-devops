5848242c211090876c910328572e1450bac64891
Abeermohsien <aboorakhalil993@gmail.com>
2024-04-04 04:12:14 -0700

    first time

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,25 @@
-#!/usr/bin/pup
-# install specific version
+# install flask from pip3.
+
+# epython
+package { 'python3.8':
+  ensure => present,
+}
+
+# pip presnting
+package { 'python3-pip':
+  ensure => present,
+}
+
+# flask install
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# package
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
