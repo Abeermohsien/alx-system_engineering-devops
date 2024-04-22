abbb923c627be80241042b3f397262a5036e66b0
Abeermohsien <aboorakhalil993@gmail.com>
2024-04-22 15:26:29 +0000

    api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""apu export"""
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """get url"""
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
+
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """completed"""
+            title_task = task.get('title')
+            """end"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""api"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    URL = "https://jsonplaceholder.typicode.com/users"
+
+    res = requests.get(URL)
+    Users = res.json()
+
+    user_dict = {}
+    for user in Users:
+        USER_ID = user.get('id')
+        USERNAME = user.get('username')
+        URL =  'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
+        URL = URL + '/todos/'
+        res = requests.get(URL)
+
+        tasks = res.json()
+        user_dict[USER_ID] = []
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            user_dict[USER_ID].append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
+            })
+            """documentation"""
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(user_dict, f)
