dfaa04a8140f5611d8dac1297fe1391c758d03eb
Abeermohsien <aboorakhalil993@gmail.com>
2024-05-07 14:45:51 +0000

    advanced api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+give num of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """gives the nber of subscribe"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        subs = data['data']['subscribers']
+        return subs
+    else:
+        return 0
