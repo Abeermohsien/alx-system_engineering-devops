8487b164db59038243dc2d68e7f1cbfdaaf517b9
Abeermohsien <aboorakhalil993@gmail.com>
2024-05-07 08:06:23 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-give num of subscribers"""
+give num of subscribers
+"""
 
 import requests
 
@@ -9,10 +10,10 @@ def number_of_subscribers(subreddit):
     """gives the nber of subscribe"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code == 200:
-        data = res.json()
-        subs = data['data']['subscribers']
-        return subs
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
